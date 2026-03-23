f089eb9af4c66f1226166647e42893c2ada94c8c
vernonthedev <techjaja2@gmail.com>
2026-03-23 11:46:49 +0300

    feat(payments): add createPayment method

--- apps/payments/src/payments/payments.service.ts
+++ apps/payments/src/payments/payments.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePaymentRequest } from './dto/create-payment.request';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  private readonly payments: any[] = [];
+
+  createPayment(request: CreatePaymentRequest) {
+    const payment: any = {
+      ...request,
+      id: Math.random().toString(36).substring(2, 15),
+    };
+    this.payments.push(payment);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return payment;
+  }
+}
